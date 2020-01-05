3d6ca434416dd145cbd1b7af7cd2fb436b8b1647
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2020-01-06 01:17:54 +0900

    Temporary fix test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -3,7 +3,13 @@ import index from '@/pages/index.vue'
 
 describe('index.vue', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(index)
+    const wrapper = mount(index, {
+      mocks: {
+        $accessor: {
+          routines: []
+        }
+      }
+    })
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
