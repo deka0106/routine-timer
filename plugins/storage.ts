666ae931ec4f3761ecc2b9631de521417c77b8a7
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2020-01-06 16:51:01 +0900

    Improve storage impl

--- plugins/storage.ts
+++ plugins/storage.ts
@@ -1,4 +1,5 @@
 import { Plugin } from '@nuxt/types'
+import { RootState } from '@/store'
 
 declare global {
   interface Window {
@@ -6,9 +7,18 @@ declare global {
   }
 }
 
-const storage: Plugin = ({ app }) => {
+const STORAGE_KEY = 'routine-timer'
+
+const storage: Plugin = ({ app, store }) => {
   window.onNuxtReady(() => {
-    app.$accessor.load()
+    const value = window.localStorage.getItem(STORAGE_KEY)
+    if (value !== null) {
+      const state: RootState = JSON.parse(value)
+      app.$accessor.replaceState(state)
+    }
+    store.subscribe(() => {
+      window.localStorage.setItem(STORAGE_KEY, JSON.stringify(store.state))
+    })
   })
 }
 
--- store/index.ts
+++ store/index.ts
@@ -8,41 +8,33 @@ import {
 
 import { Routine } from 'routine-timer'
 
-function save(state: RootState) {
-  window.localStorage.setItem('routines', JSON.stringify(state.routines))
-}
-
 export const state = (): {
   routines: Routine[]
 } => ({
   routines: []
 })
 
-type RootState = ReturnType<typeof state>
+export type RootState = ReturnType<typeof state>
 
 export const getters = getterTree(state, {})
 
 export const mutations = mutationTree(state, {
   addRoutine(state, routine: Routine) {
     state.routines.push(routine)
-    save(state)
   },
   deleteRoutine(state, index: number) {
     state.routines.splice(index, 1)
-    save(state)
   },
   updateRoutine(
     state,
     { index, routine }: { index: number; routine: Routine }
   ) {
     Vue.set(state.routines, index, routine)
-    save(state)
   },
-  load(state) {
-    const routines: Routine[] = JSON.parse(
-      window.localStorage.getItem('routines') || '[]'
-    )
-    Vue.set(state, 'routines', routines)
+  replaceState(state, newState: RootState) {
+    for (const [key, value] of Object.entries(newState)) {
+      Vue.set(state, key, value)
+    }
   }
 })
 
